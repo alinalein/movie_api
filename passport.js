d410ba365efb56c0e576f5ee0dc711ec26d9a8d0
alilein1990 <alina.leinweber@gmx.de>
2023-12-07 16:32:06 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -18,17 +18,21 @@ passport.use(
             await Users.findOne({ Username: username })
                 .then((user) => {
                     if (!user) {
-                        console.profileEnd('incorrect username');
+                        console.log('incorrect username');
                         return callback(null, false, {
                             message: 'Incorrect username or passoword.',
                         });
+                    }
+                    if (!user.validatePassword(password)) {
+                        console.log('Incorrect password');
+                        return callback(null, false, { message: 'Incorrect password.' })
                     }
                     console.log('finished');
                     return callback(null, user);
                 })
                 .catch((error) => {
                     if (error) {
-                        console.log(error);
+                        console.error(error);
                         return callback(error);
                     }
                 })
