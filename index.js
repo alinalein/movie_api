b792cadb5be7e7a2a1cf3b2c7d5598aa98f1ae37
alilein1990 <alina.leinweber@gmx.de>
2024-01-18 22:21:35 +0100

    update validation for update details

--- index.js
+++ index.js
@@ -150,7 +150,16 @@ check('Email', 'Please type a valid email').isEmail(),
 });
 
 app.put('/users/update/:Username', [check('Username', 'The user name is required and must be at least 5 characters long').isLength({ min: 5 }),
-check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric()],
+check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+check('Password', 'Please type a password').optional(), // Make password optional
+check('Email', 'Please type a valid email').isEmail().optional(),
+body('Password')
+    .optional()
+    .matches(/\d/)
+    .withMessage('Password must contain at least 1 number')
+    .matches(/[A-Za-z]/)
+    .withMessage('Password must contain at least 1 letter'),
+],
     passport.authenticate('jwt', { session: false }), async (req, res) => {
         if (req.user.Username !== req.params.Username) {
             return res.status(400).send('Permission denied!')
