9356d5472fab45f430bfed13ee5ff433da919893
alilein1990 <alina.leinweber@gmx.de>
2024-01-24 12:19:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.put('/users/update/:Username',
             const updateFields = {};
             if (req.body.Username) updateFields.Username = req.body.Username;
             if (req.body.Password) updateFields.Password = Users.hashPassword(req.body.Password);
-            if (req.body.email !== "" && req.body.email !== null) updateFields.Email = req.body.Email;
+            if (req.body.Email !== "" && req.body.Email !== null) updateFields.Email = req.body.Email;
             if (req.body.Birthday) updateFields.Birthday = req.body.Birthday;
 
             // Update the user with new values
