489809d37faada83071b1749708b3a80b3ae1eee
alilein1990 <alina.leinweber@gmx.de>
2024-09-03 13:49:25 +0200

    adjusted S3 routes

--- src/routes/s3Routes.js
+++ src/routes/s3Routes.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
-const { S3Client, ListObjectsV2Command, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3'),
+const { S3Client, ListObjectsV2Command, PutObjectCommand, GetObjectCommand, HeadObjectCommand } = require('@aws-sdk/client-s3'),
     express = require('express'),
     router = express.Router(),
     path = require('path'),
     fs = require('fs'),
-    fileUpload = require('express-fileupload');
+    fileUpload = require('express-fileupload'),
+    passport = require('passport');
 
 const IMAGES_BUCKET = process.env.BUCKET_NAME;
+
 // Define the path to the utils folder for temporary storage
 const UPLOAD_TEMP_PATH = path.join(__dirname, process.env.UPLOAD_TEMP_PATH);
 const AWS_REGION = process.env.AWS_REGION;
@@ -14,15 +16,28 @@ const AWS_REGION = process.env.AWS_REGION;
 // Add command req.files, otherwise it will not be recognized
 router.use(fileUpload());
 
-// Initialize the S3 client
+// Initialize the S3 client -> chnage after testing back !!!!
 const s3Client = new S3Client({
     region: AWS_REGION
+    // region: 'us-east-1',
+    // endpoint: 'http://localhost:4566',
+    // forcePathStyle: true
 });
 
-// Listing all objects in a S3 bucket
-router.get('/images', async (req, res) => {
+// Listing all objects in a S3 bucket depending on the specified type 
+router.get('/images/:type', passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
-        const listObjectsParams = { Bucket: IMAGES_BUCKET };
+        const type = req.params.type;
+        if (!['original', 'thumbnail'].includes(type)) {
+            return res.status(400).send('Invalid image type. Use "original" or "thumbnail".');
+        }
+
+        const folder = type === 'original' ? 'original-images' : 'resized-images';
+        const listObjectsParams = {
+            Bucket: IMAGES_BUCKET,
+            Prefix: `${folder}`
+        };
+
         const listObjectsCmd = new ListObjectsV2Command(listObjectsParams);
         const listObjectsResponse = await s3Client.send(listObjectsCmd);
         res.status(200).send(listObjectsResponse);
@@ -32,8 +47,9 @@ router.get('/images', async (req, res) => {
     }
 });
 
+
 // Uploading an object to a S3 bucket
-router.post('/images', async (req, res) => {
+router.post('/images', passport.authenticate('jwt', { session: false }), async (req, res) => {
 
     const mime = await import('mime');
 
@@ -48,16 +64,42 @@ router.post('/images', async (req, res) => {
             res.status(500).send('The file could not be uploaded')
         }
         try {
+
+            // Check if the file already exists in S3
+            const headObjectParams = {
+                Bucket: IMAGES_BUCKET,
+                // Key: `resized-images/${fileName}`
+                Key: `original-images/${fileName}`,
+            };
+            const headObjectCmd = new HeadObjectCommand(headObjectParams);
+
+            try {
+                await s3Client.send(headObjectCmd);
+                // If no error, the file exists
+                fs.unlinkSync(tempPath); // Clean up the temp file
+                return res.status(409).send({ message: `File ${fileName} already exists.` });
+            } catch (err) {
+                if (err.name !== 'NotFound') {
+                    // If the error is not "NotFound", something else went wrong
+                    console.error('Error checking if file exists in S3:', err);
+                    fs.unlinkSync(tempPath); // Clean up the temp file
+                    return res.status(500).send('An error occurred while checking the file.');
+                }
+                // If "NotFound", it means the file does not exist,  proceed
+            }
+
             // Read the file from the temporary path
             const fileData = fs.readFileSync(tempPath);
             // Determine file content type based on file extension
             const contentType = mime.default.getType(fileName) || 'application/octet-stream';
+
             // Upload the file to S3
             const uploadParams = {
                 Bucket: IMAGES_BUCKET,
-                Key: fileName,
+                Key: `original-images/${fileName}`,
+                // Key: `resized-images/${fileName}`,
                 Body: fileData,
-                ContentType: contentType
+                ContentType: contentType,
             };
             const putObjectCmd = new PutObjectCommand(uploadParams);
             await s3Client.send(putObjectCmd);
@@ -65,7 +107,7 @@ router.post('/images', async (req, res) => {
             // Delete the file from the temp path after upload
             fs.unlinkSync(tempPath);
 
-            res.status(200).send(`File ${fileName} uploaded successfully.`);
+            res.status(200).send({ message: `File ${fileName} uploaded successfully.` });
         } catch (error) {
             console.error('Error uploading file:', error);
             res.status(500).send('The file could not be uploaded');
@@ -73,14 +115,18 @@ router.post('/images', async (req, res) => {
     });
 });
 
-// Retrieving an object from a S3 bucket
-router.get('/images/:key', async (req, res) => {
+// Retrieving an object from a S3 bucket depending on the specified type 
+router.get('/images/:key(*)', passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
-        const fileKey = req.params.key;
+        const key = req.params.key;
+
+        if (!key.startsWith('original-images/') && !key.startsWith('resized-images/')) {
+            return res.status(400).send('Invalid key. The key must start with "original-images/" or "resized-images/".');
+        }
 
         const getObjectParams = {
             Bucket: IMAGES_BUCKET,
-            Key: fileKey
+            Key: key
         };
         const getObjectCmd = new GetObjectCommand(getObjectParams)
         const objectData = await s3Client.send(getObjectCmd);
@@ -89,7 +135,7 @@ router.get('/images/:key', async (req, res) => {
         res.setHeader('Content-Type', contentType);
 
         objectData.Body.pipe(res).on('finish', () => {
-            console.log(`File ${fileKey} retrieved successfully.`);
+            console.log(`File ${key} retrieved successfully.`);
         });
 
     } catch (error) {
